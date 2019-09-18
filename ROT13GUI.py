72ecb1fb308d34a03e35308c8b4455f9c4e86682
namtab10903 <wpesfriendnva@gmail.com>
2019-09-18 16:19:04 -0400

    Update ROT13GUI.py

--- ROT13GUI.py
+++ ROT13GUI.py
@@ -2,8 +2,8 @@ from tkinter import *
 from tkinter import messagebox
 
 def ROT13():
-    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
-				'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    alphabet =  'abcdefghijklmnopqrstuvwxyz'
+
     nStr = str(n.get())
     keyShift = 13
     encryptedString = []
